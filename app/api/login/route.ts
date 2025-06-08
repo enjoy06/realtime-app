2c4d91e7553a149ac30179d5d300328e8245192c
enjoy06 <152522310+enjoy06@users.noreply.github.com>
2025-06-08 22:54:57 +0700

    Update route.ts

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,29 +1,27 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 import { sign } from "jsonwebtoken";
-import { app } from "@/lib/firebase"; // pastikan sudah dikonfigurasi
-import { getFirestore, collection, getDocs, limit, query } from "firebase/firestore";
+import { db } from "@/lib/firebaseAdmin";
 
 export async function POST(req: Request) {
   const { password } = await req.json();
-  const db = getFirestore(app);
-  const q = query(collection(db, "realtime_access"), limit(1));
-  const snapshot = await getDocs(q);
+  const cek = db.collection("realtime_access").limit(1);
+  const snapshot = await cek.get();
 
   if (snapshot.empty) {
     return NextResponse.json({ error: "No credentials set" }, { status: 500 });
   }
 
   const doc = snapshot.docs[0];
-  const { password: hashedPassword } = doc.data();
+  const { password: hashedPassword, role } = doc.data();
 
   const isValid = await bcrypt.compare(password, hashedPassword);
   if (!isValid) {
     return NextResponse.json({ error: "Invalid password" }, { status: 401 });
   }
 
-  const token = sign({ role: "admin" }, process.env.JWT_SECRET!, {
-    expiresIn: "3m",
+  const token = sign({ role: role }, process.env.JWT_SECRET!, {
+    expiresIn: "1d",
   });
 
   const res = NextResponse.json({ success: true });
